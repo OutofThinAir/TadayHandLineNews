7cbf6d3f78781e9f8e88df390e73d663c50f1339
OutofThinAir <441248765@qq.com>
2017-03-11 08:29:28 +0800

    OutofThinAir 17-03-11 提交

--- ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/activity/MainActivity.java
+++ ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/activity/MainActivity.java
@@ -1,15 +1,122 @@
 package com.outofthinair.tadaynews.activity;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.outofthinair.tadaynews.R;
+import com.outofthinair.tadaynews.fragment.GuanZhuFragment;
+import com.outofthinair.tadaynews.fragment.HomeFragment;
+import com.outofthinair.tadaynews.fragment.MyFragment;
+import com.outofthinair.tadaynews.fragment.SunFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements View.OnClickListener{
+
+    private FragmentManager manager;
+    private RadioGroup group;
+    private RadioButton home;
+    private RadioButton sun;
+    private RadioButton guanzhu;
+    private RadioButton mys;
+    private HomeFragment homeFragment;
+    private SunFragment sunFragment;
+    private GuanZhuFragment guanZhuFragment;
+    private MyFragment myFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //初始化控件
+        initView();
+
+        //侧滑效果的实现
+        SlidingMenu menu = new SlidingMenu(MainActivity.this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setBehindOffset(200);
+        menu.setMenu(R.layout.cehua_layout);
+        menu.attachToActivity(MainActivity.this,SlidingMenu.SLIDING_CONTENT);
+
+        //添加Fragment
+        homeFragment = new HomeFragment();
+        sunFragment = new SunFragment();
+        guanZhuFragment = new GuanZhuFragment();
+        myFragment = new MyFragment();
+        manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.main_lay_frame, homeFragment,"hf");
+        transaction.add(R.id.main_lay_frame, sunFragment,"sf");
+        transaction.add(R.id.main_lay_frame, guanZhuFragment,"gf");
+        transaction.add(R.id.main_lay_frame, myFragment,"mf");
+        transaction.hide(sunFragment);
+        transaction.hide(guanZhuFragment);
+        transaction.hide(myFragment);
+
+        transaction.commit();
+
+        //默认选择第一个导航
+        home.setSelected(true);
+
+    }
+
+    //初始化控件
+    private void initView(){
+        group = (RadioGroup) findViewById(R.id.main_lay_rg);
+        home = (RadioButton) findViewById(R.id.main_lay_down_shouye);
+        sun = (RadioButton) findViewById(R.id.main_lay_down_yang);
+        guanzhu = (RadioButton) findViewById(R.id.main_lay_down_guanzhu);
+        mys = (RadioButton) findViewById(R.id.main_lay_down_wode);
+    }
+
+    //Fragment之间的切换
+    private void cutFragment(Fragment newFrag,Fragment oldFrag01,Fragment oldFrag02,Fragment oldFrag03){
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.show(newFrag);
+        transaction.hide(oldFrag01);
+        transaction.hide(oldFrag02);
+        transaction.hide(oldFrag03);
+        transaction.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.main_lay_down_shouye:
+                cutFragment(homeFragment,sunFragment,guanZhuFragment,myFragment);
+                home.setSelected(true);
+                sun.setSelected(false);
+                guanzhu.setSelected(false);
+                mys.setSelected(false);
+                break;
+            case R.id.main_lay_down_yang:
+                cutFragment(sunFragment,homeFragment,guanZhuFragment,myFragment);
+                home.setSelected(false);
+                sun.setSelected(true);
+                guanzhu.setSelected(false);
+                mys.setSelected(false);
+                break;
+            case R.id.main_lay_down_guanzhu:
+                cutFragment(guanZhuFragment,sunFragment,homeFragment,myFragment);
+                home.setSelected(false);
+                sun.setSelected(false);
+                guanzhu.setSelected(true);
+                mys.setSelected(false);
+                break;
+            case R.id.main_lay_down_wode:
+                cutFragment(myFragment,sunFragment,guanZhuFragment,homeFragment);
+                home.setSelected(false);
+                sun.setSelected(false);
+                guanzhu.setSelected(false);
+                mys.setSelected(true);
+                break;
+
+        }
     }
 }
--- ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/activity/WelActivity.java
+++ ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/activity/WelActivity.java
@@ -1,9 +1,13 @@
 package com.outofthinair.tadaynews.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.outofthinair.tadaynews.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Administrator on 2017/3/10.
@@ -14,5 +18,17 @@ public class WelActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wel_layout);
+        //倒计时跳转
+        Timer timer = new Timer();
+        //timer的任务栈
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(WelActivity.this,MainActivity.class));
+                finish();
+            }
+        };
+
+        timer.schedule(task,2000);
     }
 }
