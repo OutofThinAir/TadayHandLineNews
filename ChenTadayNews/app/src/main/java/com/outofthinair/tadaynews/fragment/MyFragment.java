6a7852071ceae7342b44db7a5ea1d35a886033bf
OutofThinAir <441248765@qq.com>
2017-03-15 09:34:53 +0800

    OutOfThinAir 1703150934 早

--- ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/fragment/MyFragment.java
+++ ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/fragment/MyFragment.java
@@ -6,6 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.outofthinair.tadaynews.R;
 
@@ -17,7 +22,43 @@ public class MyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
        View view = inflater.inflate(R.layout.my_frag_layout,null);
+        //初始化控件
+        initView(view);
+
+
         return view;
+    }
+
+    private void initView(View view){
+        //登录之后显示的界面
+        ImageView touxiang = (ImageView) view.findViewById(R.id.my_frag_lay_touxiang);
+        TextView uname = (TextView) view.findViewById(R.id.my_frag_lay_uname);
+        ImageView tiao_uxinxi= (ImageView) view.findViewById(R.id.my_frag_lay_next);
+        LinearLayout guanzhud = (LinearLayout) view.findViewById(R.id.my_frag_lay_loghou);
+        TextView dongtai= (TextView) view.findViewById(R.id.my_frag_lay_dongtai);
+        TextView guanzhu= (TextView) view.findViewById(R.id.my_frag_lay_guanzhu);
+        TextView fensi= (TextView) view.findViewById(R.id.my_frag_lay_fensi);
+        TextView fangke= (TextView) view.findViewById(R.id.my_frag_lay_fangke);
+
+        //一些登录按钮
+        ImageButton qqLog = (ImageButton) view.findViewById(R.id.my_frag_lay_qqlog);
+        ImageButton shoujiLog = (ImageButton) view.findViewById(R.id.my_frag_lay_shoujilog);
+        TextView genLog = (TextView) view.findViewById(R.id.my_frag_lay_gengduolog);
+        //都有的界面
+        TextView shoucang = (TextView) view.findViewById(R.id.my_frag_lay_shoucang);
+        TextView lishi = (TextView) view.findViewById(R.id.my_frag_lay_lishi);
+        TextView yejian = (TextView) view.findViewById(R.id.my_frag_lay_yejian);
+
+        RelativeLayout xiaoxi = (RelativeLayout) view.findViewById(R.id.my_frag_lay_xiaoxitongzhi);
+        RelativeLayout shangcheng = (RelativeLayout) view.findViewById(R.id.my_frag_lay_shangcheng);
+        RelativeLayout tegong = (RelativeLayout) view.findViewById(R.id.my_frag_lay_tegong);
+        RelativeLayout baolaio = (RelativeLayout) view.findViewById(R.id.my_frag_lay_boliao);
+        RelativeLayout fankui = (RelativeLayout) view.findViewById(R.id.my_frag_lay_fankui);
+        RelativeLayout xitong = (RelativeLayout) view.findViewById(R.id.my_frag_lay_xitong);
+
+
+
     }
 }
--- ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/fragment/SunFragment.java
+++ ChenTadayNews/app/src/main/java/com/outofthinair/tadaynews/fragment/SunFragment.java
@@ -2,22 +2,72 @@ package com.outofthinair.tadaynews.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.outofthinair.tadaynews.R;
+import com.outofthinair.tadaynews.adapter.SunFragmentPagerAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Administrator on 2017/3/10.
  */
 
 public class SunFragment extends Fragment {
+
+    private TabLayout tab;
+    private ViewPager pager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.sun_frag_layout,null);
+        initView(view);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //标题选项卡
+        ArrayList<String> titles =new ArrayList<>();
+        titles.add("推荐");
+        titles.add("音乐");
+        titles.add("搞笑");
+        titles.add("社会");
+        titles.add("小品");
+        titles.add("生活");
+        titles.add("影视");
+        titles.add("娱乐");
+        titles.add("呆萌");
+        titles.add("游戏");
+        titles.add("开眼");
+        titles.add("再看一遍");
+        titles.add("火山直播");
+        titles.add("原创");
+
+        //viewPager中的Fragment
+        ArrayList<Fragment> fragments = new ArrayList<>();
+        for (int i = 0; i <titles.size() ; i++) {
+            fragments.add(SunItemFragment.getSunItemFragment(titles.get(i)));
+        }
+
+        SunFragmentPagerAdapter adapter = new SunFragmentPagerAdapter(getFragmentManager(),fragments,titles);
+        pager.setAdapter(adapter);
+        tab.setupWithViewPager(pager);
+        tab.setTabsFromPagerAdapter(adapter);
+
+
+    }
+
+    //初始化控件
+    private void initView(View view){
+        tab = (TabLayout) view.findViewById(R.id.sun_lay_tab);
+        pager = (ViewPager) view.findViewById(R.id.sun_lay_pager);
     }
 }
